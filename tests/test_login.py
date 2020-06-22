972ba774304e782dcc33faad9f3de13fe4a5ffd4
Mehwish <Mehwishpeter.est@gmail.com>
2020-06-22 23:21:37 +0300

    required parameters are added

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,9 +6,9 @@ import config
 
 class TestLogIn:
 
-    def test_login(self, driver):
+    def test_login(self, driver, email, password):
         driver.find_element_by_class_name("login").click()
-        driver.find_element_by_id("email").send_keys(config.email)
-        driver.find_element_by_id("passwd").send_keys(config.password)
+        email = driver.find_element_by_id("email").send_keys(config.email)
+        password = driver.find_element_by_id("passwd").send_keys(config.password)
         driver.find_element_by_id("SubmitLogin").click()
         time.sleep(5)
