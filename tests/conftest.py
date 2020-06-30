40698f57ae291fd935032296db59b6a0adb356cc
Mehwish93 <36604763+Mehwish93@users.noreply.github.com>
2020-06-30 21:05:45 +0300

    Packages (#4)
    * Added files to packages folder
    
    * Data added in confest file
    
    * Login Testcase has been added along with different methods in conftest
    
    * added required parameter
    
    * required parameters are added
    
    * required parameters are added
    
    * All the asked changes are done
    
    * logout method has been added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,45 @@
-#Git hub Demo 
+import pytest
+
+import config
+from tests.test_login import TestLogin
+from utilities.driver_factory import Driver
+
+d = Driver()  # Created an instance
+
+
+@pytest.fixture(scope="session")
+def driver(browser):
+    driver = d.launch_url(browser)
+    # todo, teardown
+    return driver
+
+
+@pytest.fixture(scope="session", autouse=True)  # autouse will help to run the test case automatically
+def browser(request):
+    return request.config.getoption("--browser")
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", default="chrome")
+    parser.addoption("--email", required=True)
+    parser.addoption("--password", required=True)
+    parser.addoption("--logout", required=False)
+
+
+
+@pytest.fixture(scope="session", autouse=True)
+def email(request):
+    return request.config.getoption("--email")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def password(request):
+    return request.config.getoption("--password")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def logout(request):
+    return request.config.getoption("--logout")
+
+
+
---
+++ utilities/driver_factory.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import IEDriverManager
+from webdriver_manager.opera import OperaDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+import config
+
+
+class Driver:
+    def launch_url(self, browser):
+        driver = None
+        browser_lower = browser.lower() #lower() is a methodwhch
+        if browser_lower == "chrome":
+            driver = webdriver.Chrome(ChromeDriverManager().install())
+        elif browser_lower == "firefox":
+            driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        elif browser_lower == "ie":
+            driver = webdriver.Ie(IEDriverManager().install())
+        elif browser_lower == "opera":
+            driver = webdriver.Opera(executable_path=OperaDriverManager().install())
+        elif browser_lower == "edge":
+            driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+        driver.get(config.url)
+        driver.maximize_window()
+        return driver
+
+    def quit_driver(self, driver):
+        driver.quit_driver()
