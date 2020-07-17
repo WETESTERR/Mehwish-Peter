b1b3cf410dc07aa2e829e105b211c1178a29aaa4
Mehwish <Mehwishpeter.est@gmail.com>
2020-07-17 13:46:29 +0300

    Amenments addes in log file

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 
 url = "http://automationpractice.com/index.php"
+path = "C:\\Users\\AY54109\\Desktop\\Automation.project\\Mehwish-Peter\\utilities\\logfile.log"
--- utilities/log.py
+++ utilities/log.py
@@ -1,22 +1,35 @@
 import logging
 
+import os
 
-def Caseslogs():
-    logger = logging.getLogger(__name__)
-      #file handler is an other class which needs to be pass in addhandler method
-    #filehandler is used for file location
+class Loggs:
 
-    logfile = logging.FileHandler('testlogs.log')
-    stream_handler = logging.StreamHandler()
-    logger.addHandler(logfile)
-    logger.addHandler(stream_handler)
+    def loggers(self):
+        logger = logging.getLogger(__name__)
+        #file handler is an other class which needs to be pass in addhandler method
+        #filehandler is used for file location
+        directory = "C:\\Users\\AY54109\\Desktop\\Automation.project\\Mehwish-Peter\\utilities"
+        os.mkdir("logfile.log")
+
+        if not os.path.exists(directory):
+            os.mkdir(directory)
 
 
-    logformat = logging.Formatter("%(asctime)s : %(levelname)s :%(name)s :%(message)s")
-    logfile.setFormatter(logformat)
+        logfile = logging.FileHandler('testlogs.log')
+        stream_handler = logging.StreamHandler()
+        logger.addHandler(logfile)
+        logger.addHandler(stream_handler)
 
-    logger.info("Check the minior issues in your code")
-    logger.debug("Code check Debug")
-    logger.warning("The issue needs to resolve as soon as posible")
-    logger.critical("The issue  is severe and need to fixed")
-    logger.error("An Error")
+
+        logformat = logging.Formatter("%(asctime)s : %(levelname)s :%(name)s :%(message)s")
+        logfile.setFormatter(logformat)
+
+        logger.info("Check the minior issues in your code")
+        logger.debug("Code check Debug")
+        logger.warning("The issue needs to resolve as soon as posible")
+        logger.critical("The issue  is severe and need to fixed")
+        logger.error("An Error")
+
+
+
+        "Mehwish Peter Testing for Remi For Finniar"
